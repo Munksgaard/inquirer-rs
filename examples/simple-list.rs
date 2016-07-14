290bc80cd77a8fc3c013ec465af74513d3f49923
Pascal Hertleif <killercup@gmail.com>
2016-07-15 00:33:25 +0200

    Use `.enumerate` in simple-list example
    Sadly, not so simple anymore :/

--- examples/simple-list.rs
+++ examples/simple-list.rs
@@ -3,7 +3,12 @@ extern crate inquirer;
 use inquirer::list;
 
 fn main() {
-    let choices =  &["An option", "Another option", "Something else?"];
-    let result = list("Choose an option:", choices).unwrap();
+    let choices: Vec<_> = vec!["An option", "Another option", "Something else?"].into_iter()
+        .enumerate()
+        .map(|(index, item)| (item, index))
+        .collect();
+
+    let result = list("Choose an option:", &choices).unwrap();
+
     println!("You chose {:?}.", result);
 }
